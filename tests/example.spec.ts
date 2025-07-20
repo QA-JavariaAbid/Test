d525f6d9eaed84f043509bc22528e7ac408f1106
QA-JavariaAbid <javariaabid079@gmail.com>
2025-07-20 18:08:53 +0500

    4

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -4,5 +4,5 @@ test('has title', async ({ page }) => {
   await page.goto('https://google.com/');
   await expect(page).toHaveTitle('sss');
 //   // Expect a title "to contain" a substring.
- console.log('1')
+ console.log('12')
 });
